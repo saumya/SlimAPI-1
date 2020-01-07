c2e64815085b588bd683140d159dd0c62afb9b97
saumya <2saumyaray@gmail.com>
2020-01-07 13:47:33 +0530

    Code cleanup

--- public/util/util_model.php
+++ public/util/util_model.php
@@ -16,25 +16,11 @@ class UtilModel
 		$this->init();
 	}
 	private function init(){
-		/*
-		R::setup( 'mysql:host=localhost;dbname=my_red_bean_php_database', 'app_user', 'app_pw' );
-		R::useFeatureSet( 'novice/latest' );
-		//
-		$post = R::dispense( 'post' );
-    $post->title = 'My holiday';
-    $id = R::store( $post );
-    */
 
 		$this->$rbp::setup( 'mysql:host=localhost;dbname=my_red_bean_php_database', 'app_user', 'app_pw' );
 		$this->$rbp::useFeatureSet( 'novice/latest' );
 		//R::freeze( TRUE );
 		//$this->rbp::freeze( TRUE ); // makes the DB not Fixed. Nothing will change in DB Structure
-
-		/*
-		$post = $this->$rbp::dispense( 'post' );
-    $post->title = 'My holiday - Nice';
-    $id = $this->$rbp::store( $post );
-    */
 	}
 
 	public function saveTest(){
